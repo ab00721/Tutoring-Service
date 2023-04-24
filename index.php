f5c21ea3d6b19ef4a2ba2abe1037b61403ee7819
Anna Blood <ab00721@my.westga.edu>
2023-04-24 15:23:21 -0400

    moved secure connection to controller

--- index.php
+++ index.php
@@ -5,12 +5,4 @@ require_once 'Controller.php';
 $controller = new Controller();
 $controller->invoke();
 
-$https = filter_input(INPUT_SERVER, 'HTTPS');
-if (!$https) {
-    $host = filter_input(INPUT_SERVER, 'HTTP_HOST');
-    $uri = filter_input(INPUT_SERVER, 'REQUEST_URI');
-    $url = 'https://' . $host . $uri;
-    header("Location: " . $url);
-    exit();
-}
 ?>
