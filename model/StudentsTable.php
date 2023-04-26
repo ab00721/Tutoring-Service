477af62c4185434ccb7ddb9b4525797e7967fadc
Anna Blood <ab00721@my.westga.edu>
2023-04-26 17:51:42 -0400

    Added StudentsTable and addStudents() and getStudents()

---
+++ model/StudentsTable.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'Database.php';
+
+class StudentsTable {
+    private $db;
+    
+    public function __construct($db) {
+        $this->db = $db;
+    }
+          
+    /**
+     * Checks if the specified student already exists 
+     */
+    public function getStudent($parentID, $studentName) {
+        $query = 'SELECT * FROM students 
+                    WHERE parentID = :parentID
+                    AND studentName = :studentName';
+        $statement = $this->db->getDB()->prepare($query);
+        $statement->bindValue(':parentID', $parentID);
+        $statement->bindValue(':studentName', $studentName);
+        $statement->execute();
+        $student = $statement->fetch();
+        $statement->closeCursor();
+        return $student;
+    }
+    
+    /**
+     * Adds the specified student to the table parents
+     */
+    public function addStudent($parentID, $studentID, $studentName) {
+        $query = 'INSERT INTO students (parentID, studentID, studentName)
+              VALUES (:parentID, :studentID, :studentName)';
+        $statement = $this->db->getDB()->prepare($query);
+        $statement->bindValue(':parentID', $parentID);
+        $statement->bindValue(':studentID', $studentID);
+        $statement->bindValue(':studentName', $studentName);
+        $statement->execute();
+        $statement->closeCursor();
+    }
+    
+}
